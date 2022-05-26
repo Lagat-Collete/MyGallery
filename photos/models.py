fe805d4ed76e5c562678c55eea71b2552ef15226
colletemine <jepngetichcolleta@gmail.com>
2022-05-26 17:58:21 +0300

    Adding models to admin page

--- photos/models.py
+++ photos/models.py
@@ -10,20 +10,36 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def save_category(self):
+        self.save()
+
 class Location(models.Model):
     name = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
 
+    def save_location(self):
+        self.save()
+
 
 class Image(models.Model):
     image = models.ImageField(upload_to =' ')
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=40)
-    location = models.ForeignKey(Location)
-    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
     posted_date = models.DateTimeField(auto_now_add=True)
+
+
+
+    def __str__(self):
+        return self.name
+
+    def save_Image(self):
+        self.save()
+
+
 
 
 
--- photos/tests.py
+++ photos/tests.py
@@ -2,3 +2,12 @@ from django.test import TestCase
 from .models import Image,Category,Location
 
 # Create your tests here.
+class CtegoryTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.wild= Category(name= 'wild')
+
+      
+
+
