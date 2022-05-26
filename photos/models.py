2b216806b542e2d3d4309e619851a79c97792868
colletemine <jepngetichcolleta@gmail.com>
2022-05-26 17:09:57 +0300

    Add model classes and their properties

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from distutils.command.upload import upload
+import datetime as dt
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Location(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to =' ')
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=40)
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+    posted_date = models.DateTimeField(auto_now_add=True)
+
+
+
+
+
