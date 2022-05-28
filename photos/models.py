1899840da64fa3db5d4377978daf5282b2a07e7d
colletemine <jepngetichcolleta@gmail.com>
2022-05-28 17:59:55 +0300

    Change fieldtext area in image model

--- photos/models.py
+++ photos/models.py
@@ -61,7 +61,7 @@ class Image(models.Model):
 
     @classmethod
     def search_by_category(cls,search_term):
-        images = Image.objects.filter(category__icontains=search_term)
+        images = cls.objects.filter(category__icontains=search_term)
         return images
     
     @classmethod
