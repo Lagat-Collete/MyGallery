fc742d9ab55505c0519e74acc187b26fd82e3e87
colletemine <jepngetichcolleta@gmail.com>
2022-05-28 22:33:29 +0300

    test category class

--- photos/tests.py
+++ photos/tests.py
@@ -1,12 +1,27 @@
+from unicodedata import category
 from django.test import TestCase
 from .models import Image,Category,Location
 
 # Create your tests here.
-class CtegoryTestClass(TestCase):
+class CategoryTestClass(TestCase):
 
     # Set up method
     def setUp(self):
-        self.wild= Category(name= 'wild')
+        self.category= Category(name= 'trees')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category,Category))
+
+    def test_save_category(self):
+        self.category.save_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category)>0)
+
+    def test_delete_category(self):
+        self.category.save_category()
+        self.category.delete_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category)==0)
 
       
 
